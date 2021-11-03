be5dbc2554aa023ea4050043c20730c8eacb67a6
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-11-03 21:37:35 +0300

    server running

--- server.js
+++ server.js
@@ -3,3 +3,24 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./configs/config.env" });
 
 const app = require("/app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB Connected Succesfully");
+  });
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
