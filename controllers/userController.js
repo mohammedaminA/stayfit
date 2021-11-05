49926450c3f89cfc289adfa4822a2e1bd2588f42
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-11-05 22:20:11 +0300

    dummy userController

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined for now, Sorry!",
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined for now, Sorry!",
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined for now, Sorry!",
+  });
+
+  exports.updateUser = (req, res) => {
+    res.status(500).json({
+      status: "error",
+      message: "This route is not defined for now, Sorry!",
+    });
+  };
+};
