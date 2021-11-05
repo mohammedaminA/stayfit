63a425745529467a689db854c591819748149003
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-11-05 23:18:39 +0300

    fixed async error on password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = bcrypt.hash(this.password, 12);
   this.passwordConfirmed = undefined;
