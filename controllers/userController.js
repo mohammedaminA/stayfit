9768ad1ca2221665d4b2292ee5025408a68c3e7c
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-12-05 01:57:00 +0300

    removed unnecessary methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,31 +17,3 @@ exports.getAllUsers = async (req, res) => {
     });
   }
 };
-
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined for now, Sorry!",
-  });
-};
-
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined for now, Sorry!",
-  });
-};
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined for now, Sorry!",
-  });
-};
-
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined for now, Sorry!",
-  });
-};
